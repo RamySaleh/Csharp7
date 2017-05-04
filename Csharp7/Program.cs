fe3a37ba1ae96cb11ad8a2d4bb188838b27038ed
RSaleh <Ramy.saleh.me@gmail.com>
2017-05-04 15:08:53 +0200

    static using

--- Csharp7/Program.cs
+++ Csharp7/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Csharp7
 {
@@ -10,6 +11,16 @@ namespace Csharp7
     {
         static void Main(string[] args)
         {
+            // Static using demo
+            new StaticUsing().CallStaticMethodWithoutClassName();
+            WriteLine("----------------------------");
+
+            // Out variable demo
+            new OutVariable().RunOldWay();
+            new OutVariable().RunNewWay();
+            WriteLine("----------------------------");
+
+            ReadKey();
         }
     }
 }
---
+++ Csharp7/StaticUsing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Math;
+
+namespace Csharp7
+{
+    class StaticUsing
+    {
+        public void CallStaticMethodWithoutClassName()
+        {
+            var number = 4;
+            WriteLine($"Calling Math.Sqrt on value ({number}) without writing class name = {Sqrt(number)}");
+        }
+    }
+}
